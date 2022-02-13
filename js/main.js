fa816663c9b7c6726fdecfda5b1af7968e3adfb5
Yuriy Shishkin <ya.shishkan@gmail.com>
2022-02-13 20:48:44 +0700

    ДЗ № 3

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-alert('Hello world!');
+const swiper = new Swiper(".main-slider", {
+	slidesPerView: 2,
+	spaceBetween: 28,
+  loop: true,
+  navigation: {
+    nextEl: ".main-slider__arrow",
+  },
+});
